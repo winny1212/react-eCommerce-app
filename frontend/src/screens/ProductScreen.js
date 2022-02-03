45c03865a6b6aa2ef9794b924e5e9f2a49221632
winny1212 <maomaodragon1212@gmail.com>
2022-02-04 10:21:14 +1100

    display the single product info

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -11,6 +11,7 @@ import {
   Form,
 } from 'react-bootstrap';
 import { products } from '../data';
+import Rating from '../components/Rating';
 
 const ProductScreen = () => {
   //get the product id
@@ -18,28 +19,37 @@ const ProductScreen = () => {
 
   //get the product
   const product = products.find((product) => product._id === params.id * 1);
-
   //   console.log(product);
+
   return (
     <div>
+      {/* go back to the homepage */}
       <Link className='btn btn-secondary my-3' to='/'>
         Back
       </Link>
+      {/*display the single product details  */}
       <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} />
+        {/* display image */}
+        <Col md={4}>
+          <Image src={product.image} alt={product.name} class='float-right' />
         </Col>
-        <Col md={3}>
+
+        {/* display image details */}
+        <Col md={5}>
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
-            <ListGroup.Item></ListGroup.Item>
-            <ListGroup.Item>价格：¥{product.price}</ListGroup.Item>
-            <ListGroup.Item>描述：¥{product.description}</ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} comments`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price：${product.price}</ListGroup.Item>
+            <ListGroup.Item>Details：{product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={3}></Col>
       </Row>
     </div>
   );
