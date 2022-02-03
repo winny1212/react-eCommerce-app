9fe27ef58474ffce5e21b3068b7b2eda8ee5faee
winny1212 <maomaodragon1212@gmail.com>
2022-02-03 23:03:13 +1100

    set up the product detail page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+  Form,
+} from 'react-bootstrap';
+import { products } from '../data';
 
 const ProductScreen = () => {
-  return <div>the details of produc</div>;
+  //get the product id
+  const { id } = useParams;
+  //get the product
+  const product = products.find((product) => product._id === id * 1);
+
+  return (
+    <div>
+      <Link className='btn btn-secondary my-3' to='/'>
+        Back To Homepage
+      </Link>
+      <Row>
+        <Col md={6}></Col>
+        <Col md={3}></Col>
+        <Col md={3}></Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ProductScreen;
