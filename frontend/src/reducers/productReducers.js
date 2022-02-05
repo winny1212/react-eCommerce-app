1de8fcffef370e96a54f8ef5c59e960b64405fb0
winny1212 <maomaodragon1212@gmail.com>
2022-02-05 23:30:41 +1100

    function for get products reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,1 +1,22 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/actionTypes';
 
+//request for all products
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    //  when get request
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    // when get products successfully
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    //when get products fail
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
