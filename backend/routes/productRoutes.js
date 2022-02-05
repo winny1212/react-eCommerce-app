18e44a6683f51e5fbd6ecdfd460232bc5ce7e90c
winny1212 <maomaodragon1212@gmail.com>
2022-02-05 16:57:48 +1100

    async function for getting single product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,9 @@ import asyncHandler from 'express-async-handler';
 const router = express.Router();
 import Product from '../models/productModel.js';
 
-//create the get all products route
+//@desc    get all products in database
+//@route   GET/api/products
+//@access  public
 router.get(
   '/',
   asyncHandler(async (req, res) => {
@@ -12,10 +14,19 @@ router.get(
   })
 );
 
-//create the single products route
-router.get('/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+//@desc    get single product
+//@route   GET/api/products/:id
+//@access  public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: '查询不到这个产品！' });
+    }
+  })
+);
 
 export default router;
