0bd2ab92853d732a5a9db3626a729aeb5035c4b4
winny1212 <maomaodragon1212@gmail.com>
2022-02-05 12:58:57 +1100

    insert dummy data in the database

--- backend/seeder.js
+++ backend/seeder.js
@@ -8,5 +8,34 @@ import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
 import connectDB from './config/db.js';
 
+//connect to database
 dotenv.config();
 connectDB();
+
+//insert the dummy data
+const importData = async () => {
+  try {
+    //clear database before inserting
+    await Order.deleteMany();
+    await User.deleteMany();
+    await Product.deleteMany();
+
+    //insert dummy data
+    const createdUsers = await User.insertMany(users);
+    // get the admin user
+    const adminUser = createdUsers[0]._id;
+    //insert the admin user to the product
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log('data insert succeffully！'.green.inverse);
+
+    // exit the process after inserting
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+  }
+};
