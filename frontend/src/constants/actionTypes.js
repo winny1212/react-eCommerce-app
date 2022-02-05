13c5b70c3636cb9948489f57a98b279474140df9
winny1212 <maomaodragon1212@gmail.com>
2022-02-05 23:27:13 +1100

    create the constants file for reducer

---
+++ frontend/src/constants/actionTypes.js
@@ -0,0 +1,4 @@
+//action type:products request
+export const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST';
+export const PRODUCT_LIST_SUCCESS = 'PRODUCT_LIST_SUCCESS';
+export const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL';
