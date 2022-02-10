58100921816f66a28fac7338629669fa26aa8453
winny1212 <maomaodragon1212@gmail.com>
2022-02-10 15:22:36 +1100

    JWT setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-
+  //  if user exist and the password is right, we will get the user info
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
