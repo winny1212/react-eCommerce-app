c984e5bb587b4d9739b35d1a79b0d6d704ae6b4f
winny1212 <maomaodragon1212@gmail.com>
2022-02-10 16:34:57 +1100

    function for bcrypt password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,6 +25,15 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+//function for bcrypt user's password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 // compare the password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
