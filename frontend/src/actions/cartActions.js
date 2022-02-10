da9c6d62d034b33e7f12d456810c87c1052fc427
winny1212 <maomaodragon1212@gmail.com>
2022-02-10 11:13:29 +1100

    action of add item to cart

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,23 @@
+import {
+  ADD_ITEM_TO_CART,
+  REMOVE_ITEM_FROM_CART,
+} from '../constants/actionTypes';
+import axios from 'axios';
+//add item to cart action
+export const addToCart = (id, quantity) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: ADD_ITEM_TO_CART,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      quantity,
+    },
+  });
+  //store the cart items in local
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer } from './reducers/productsListReducers';
 import { productDeatilsReducer } from './reducers/productDetailsReducer';
+import { cartReducer } from './reducers/cartReducers';
 
 //combine all reducers
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDeatilsReducer,
+  cart: cartReducer,
 });
 
 //initial state
