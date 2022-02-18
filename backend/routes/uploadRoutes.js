21b0e5a7b5be6db8d31ee621d4136787e8a3e553
winny1212 <maomaodragon1212@gmail.com>
2022-02-18 12:41:48 +1100

    image upload backend

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,48 @@
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
+
+//create disk to store
+const storage = multer.diskStorage({
+  destionaton(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname} - ${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+//validate file type
+const checkFileType = (file, cb) => {
+  //make the file type
+  const filtTypes = /jpg|jpeg|png/;
+  //check the extension name of file
+  const extname = filtTypes.test(path.extname(file.originalname).toLowerCase());
+  //
+  const mimetype = filtTypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Only image format！');
+  }
+};
+
+//upload file
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+//upload file route
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
