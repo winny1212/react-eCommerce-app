427eaf8e2f590f24818fdd7657e5c25510108a66
winny1212 <maomaodragon1212@gmail.com>
2022-02-21 19:37:11 +1100

    fix deploy error

--- backend/server.js
+++ backend/server.js
@@ -34,17 +34,14 @@ app.use('/api/upload', uploadRoutes);
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
+app.get('/', (req, res) => {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
-} else {
-  app.get('/', (req, res) => {
-    res.send('Server is running...');
-  });
-}
+});
+
 //middleware for error handling
 app.use(notFound);
 app.use(errorHandler);
