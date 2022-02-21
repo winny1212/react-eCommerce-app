e0c3d2e17a3d043d36e7c3483c8b0785269956d7
winny1212 <maomaodragon1212@gmail.com>
2022-02-21 20:04:47 +1100

    fix deoy rror

--- backend/server.js
+++ backend/server.js
@@ -30,21 +30,26 @@ app.use('/api/upload', uploadRoutes);
 
 //set upload file to static file
 const __dirname = path.resolve();
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
-});
+} else {
+  app.get('/', (req, res) => {
+    res.send('Server is running...');
+  });
+}
 
 //middleware for error handling
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(
   PORT,
   console.log(
